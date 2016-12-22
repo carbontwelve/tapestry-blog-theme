a262c810696cf68fb98874304a38e96679944ac8
Simon Dann <simon.dann@halesway.co.uk>
2016-12-22 14:00:52 +0000

    :tada: Migrated test project from carbontwelve/tapestry as per carbontwelve/tapestry/issues/27

---
+++ kernel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Site;
+
+use Tapestry\Modules\Kernel\KernelInterface;
+use Tapestry\Tapestry;
+
+class kernel implements KernelInterface
+{
+    /**
+     * @var Tapestry
+     */
+    private $tapestry;
+
+    /**
+     * DefaultKernel constructor.
+     *
+     * @param Tapestry $tapestry
+     */
+    public function __construct(Tapestry $tapestry)
+    {
+        $this->tapestry = $tapestry;
+    }
+
+    public function register()
+    {
+        // ...
+    }
+
+    public function boot()
+    {
+        // ...
+    }
+}
---
+++ source/_blog/2015-08-22-first-post.md
@@ -0,0 +1,9 @@
+---
+title: First Post
+draft: true
+categories:
+    - misc
+    - first post
+---
+
+This is some test text.
